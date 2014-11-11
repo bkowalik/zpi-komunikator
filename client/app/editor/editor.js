2c097ba21a04a395f65229ded1952fde3fc6704f
piotrbrudny <piotrbrudny@bitbucket.org>
2014-11-11 18:18:59 +0100

    Autosend on enter in conversation window.

--- client/app/editor/editor.js
+++ client/app/editor/editor.js
@@ -18,6 +18,20 @@ angular.module('developerCommunicator.editor', ['ngRoute', 'ui.bootstrap'])
                 );
             }])
 
+    .directive('ngEnter', function() {
+                   return function(scope, element, attrs) {
+                       element.bind("keydown keypress", function(event) {
+                           if(event.which === 13) {
+                               scope.$apply(function(){
+                                   scope.$eval(attrs.ngEnter, {'event': event});
+                               });
+
+                               event.preventDefault();
+                           }
+                       });
+                   };
+               })
+
     .controller('contactsController', function ($scope, $rootScope) {
                     $rootScope.contactsOnline = [
                         {
