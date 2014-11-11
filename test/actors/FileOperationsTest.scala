bd8b2a16eee4d57d58e63456d08378d734097d9f
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-11-11 16:57:42 +0100

    #21 FileActor test

---
+++ test/actors/FileOperationsTest.scala
@@ -0,0 +1,71 @@
+package actors
+
+import java.security.MessageDigest
+
+import actors.FileProtocol.{Text, GetText, DiffFromClient, AddClient}
+import akka.actor.{Props, Actor, ActorLogging, ActorSystem}
+import akka.testkit.{ImplicitSender, TestKit}
+import com.sksamuel.diffpatch.DiffMatchPatch
+import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpecLike}
+import scala.concurrent.duration._
+
+//
+//class DummyActor extends Actor with ActorLogging {
+//  def receive = {
+//    case foo => log.info(foo.toString)
+//  }
+//}
+
+class FileOperationsTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with FlatSpecLike with Matchers with BeforeAndAfterAll {
+  def this() = this(ActorSystem("file-test"))
+
+  def md5(s: String): String = {
+    MessageDigest.getInstance("MD5").digest(s.getBytes).map("%02X".format(_)).mkString
+  }
+
+  lazy val dmp = new DiffMatchPatch
+
+  it should "create diff and patch" in {
+    val old = "Macs had the original point and click UI."
+    val `new` = "Macintoshes had the original point and click interface."
+
+    val patch = dmp.patch_make(old, `new`)
+    val ret = dmp.patch_apply(patch, old)
+
+    val patchedText = ret(0).asInstanceOf[String]
+
+    patchedText shouldEqual `new`
+  }
+
+  val oldSource =
+    """
+      |case class Foo(bar: String, baz: Map[Int, ActorRef]) extends FooTrait
+      |case class Wibble(wobble: Int) extends WibbleTrait
+    """.stripMargin
+
+  val newSource =
+    """
+      |case class Foo(bar: String, baz: Map[Int, Wibble]) extends FooTrait
+      |class Wibble(wobble: Double) extends WibbleTrait
+    """.stripMargin
+
+  it should "work with FileActor" in {
+    val fileActor = system.actorOf(FileActor.props(oldSource, Map.empty), "fileActor")
+
+    val maniek = system.actorOf(Props[DummyActor], "maniek")
+    val zenek = system.actorOf(Props[DummyActor], "zenek")
+
+    fileActor ! AddClient(maniek)
+    fileActor ! AddClient(zenek)
+
+    val patchText = dmp.patch_toText(dmp.patch_make(oldSource, newSource))
+
+    fileActor ! DiffFromClient(maniek, patchText, md5(newSource))
+
+    fileActor ! GetText
+
+    val response = receiveOne(5 seconds).asInstanceOf[Text].text
+
+    response shouldEqual newSource
+  }
+}
