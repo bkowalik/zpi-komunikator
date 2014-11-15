ed29ee05a31fcfe446f851691066ef01f524e4b8
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-11-15 21:11:21 +0100

    #23 Client package creation
    make_deb.sh script works on my machine, dunno about others.

---
+++ make_deb.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# skrypt zakłada, że "bower install" zostało wykonane
+# i wszystkie zależności są już ściągnięte
+
+
+pushd client
+
+zip -r my_application.nw *
+cat /usr/bin/nw my_application.nw > devcom_client
+
+popd
+
+mkdir devcom
+pushd devcom
+
+mkdir DEBIAN
+mkdir -p opt/devcom
+mkdir -p usr/bin
+
+cp ../client/devcom_client opt/devcom/devcom
+cp /usr/lib/node-webkit/nw.pak opt/devcom/nw.pak
+
+cat > usr/bin/devcom <<EOF
+#!/bin/sh
+
+/opt/devcom/devcom
+EOF
+
+cat > DEBIAN/control <<EOF
+Source: devcom
+Package: devcom
+Version: 0.1
+Section: misc
+Priority: optional
+Architecture: all
+Maintainer: mgw
+Description: Developers' communicator
+ stub
+EOF
+
+popd
+
+dpkg-deb -b devcom
+
