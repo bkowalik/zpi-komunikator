7ae20444b692d8bad778b53099d0a934eb98a46e
Bartosz Kowalik <bartekviper@gmail.com>
2014-11-15 16:19:47 +0100

    #17 serialization fix

--- app/actors/ClientTalkActor.scala
+++ app/actors/ClientTalkActor.scala
@@ -24,16 +24,18 @@ class ClientTalkActor(val name: String, out: ActorRef, val manager: ActorRef) ex
 
     case FileProtocol.Diff(id, changer, text) =>
       log.debug(s"Received diff from $id")
-      out ! new Envelope(Set.empty, Option(id), MessageTypes.DiffSyncType, Json.toJson(Diff(text))) with EDated with ESender {
+      val msg = new Envelope(Set.empty, Option(id), MessageTypes.DiffSyncType, Json.toJson(Diff(text))) with EDated with ESender {
         val date: DateTime = new DateTime()
         val from: String = changer
       }
+      out ! Json.toJson(msg)
 
     case FileProtocol.Text(id, text) =>
       log.debug(s"Received text from $id")
-      out ! new Envelope(Set.empty, Option(id), MessageTypes.DiffSyncType, Json.toJson(Text(text))) with EDated {
+      val msg = new Envelope(Set.empty, Option(id), MessageTypes.DiffSyncType, Json.toJson(Text(text))) with EDated {
         val date: DateTime = new DateTime()
       }
+      out ! Json.toJson(msg)
 
     case unknown => log.warning(s"Unknown message: ${unknown.toString}")
   }
