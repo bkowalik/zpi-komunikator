2f38d52711208c43fc15eb9ab5c51c8566212587
Andrzej <andrz.zuzak@gmail.com>
2014-11-25 23:17:46 +0100

    changing password works

--- client/app/settings/settings.js
+++ client/app/settings/settings.js
@@ -9,6 +9,30 @@ angular.module('developerCommunicator.settings', ['ngRoute'])
                 });
             }])
 
-    .controller('settingsController', function () {
+    .controller('settingsController', function ($scope, $http) {
 
+                    $scope.tadam = function () {
+                        console.log("Tadam");
+                    };
+
+                    $scope.changePassword = function() {
+                        if($scope.new == $scope.new2) {
+                            $http.put(
+                                'http://54.77.232.158:9000/users/password',
+                                {
+                                    new: $scope.new,
+                                    old: $scope.old
+                                })
+                                .success(function (data, status, headers, config) {
+                                    $scope.new = "";
+                                    $scope.new2 = "";
+                                    $scope.old = "";
+                                })
+                                .error(function (data, status, headers, config) {
+                                    alert("Error while changing password");
+                                });
+                        } else {
+                            alert("New password must be the same in both fields!");
+                        }
+                    }
                 });
